69b92237c9234886d42b33f88dfd4a2703397b99
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:26:42 -0700

    Fixes issues where extended plugins did not get registered items.

--- cron-jobs.php
+++ cron-jobs.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'cron_jobs', [
-			'instance' => 'Underpin_Cron_Jobs\Abstracts\Cron_Job',
-			'default'  => 'Underpin_Cron_Jobs\Factories\Cron_Job_Instance',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job_Instance.php' );
+	$instance->loaders()->add( 'cron_jobs', [
+		'instance' => 'Underpin_Cron_Jobs\Abstracts\Cron_Job',
+		'default'  => 'Underpin_Cron_Jobs\Factories\Cron_Job_Instance',
+	] );
 } );
