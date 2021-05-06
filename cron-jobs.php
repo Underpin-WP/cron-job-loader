12d3b8e525710934f5ac309231469c60bf2397c9
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:15:08 -0700

    Corrects underpin autoloader

--- cron-jobs.php
+++ cron-jobs.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job_Instance.php' );
-	Underpin\underpin()->get( $file )->loaders()->add( 'cron_jobs', [
+	Underpin\underpin()->get( $file, $class )->loaders()->add( 'cron_jobs', [
 		'instance' => 'Underpin_Cron_Jobs\Abstracts\Cron_Job',
 		'default'  => 'Underpin_Cron_Jobs\Factories\Cron_Job_Instance',
 	] );
-} );
+}, 10, 2 );
