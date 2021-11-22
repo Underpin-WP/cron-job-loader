28ebe4ad7f941ec7ca8212d47123f3375b2d6828
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:03:54 -0600

    Compat for Underpin 2.0.0

--- cron-jobs.php
+++ cron-jobs.php
@@ -1,14 +1,19 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'cron_jobs', [
+	'update' => function ( Underpin $plugin ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Cron_Job_Instance.php' );
-	Underpin\underpin()->get( $file, $class )->loaders()->add( 'cron_jobs', [
+	$plugin->loaders()->add( 'cron_jobs', [
 		'instance' => 'Underpin_Cron_Jobs\Abstracts\Cron_Job',
 		'default'  => 'Underpin_Cron_Jobs\Factories\Cron_Job_Instance',
 	] );
-}, 4, 2 );
+	},
+] ) );
