afb7c56902f5a45a9ff7bc77cfdfdc983d88bf18
Alex Standiford <a@alexstandiford.com>
2021-11-24 15:32:11 -0600

    Fixes loader

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add this loader.
 Underpin::attach( 'setup', new \Underpin\Factories\Observers\Loader( 'cron_jobs', [
-	'instance' => 'Underpin\Cron_Jobs\Abstracts\Cron_Job',
-	'default'  => 'Underpin\Cron_Jobs\Factories\Cron_Job_Instance',
+	'abstraction_class' => 'Underpin\Cron_Jobs\Abstracts\Cron_Job',
+	'default_factory'  => 'Underpin\Cron_Jobs\Factories\Cron_Job_Instance',
 ] ) );
